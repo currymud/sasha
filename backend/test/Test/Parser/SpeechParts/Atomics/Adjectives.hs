0a52103b63268623e6971eb3905f553c1c1954ba
Michael Litchard <michael.litchard@platonic.systems>
2025-06-17 17:30:29 +0000

    wip

---
+++ backend/test/Test/Parser/SpeechParts/Atomics/Adjectives.hs
@@ -0,0 +1,12 @@
+module      Test.Parser.SpeechParts.Atomics.Adjectives where
+import           Parser.SpeechParts.Atomics.Adjectives (adjectives)
+import           Test.Hspec                            (Spec, describe, hspec)
+import           Test.Hspec.QuickCheck                 (prop)
+import           Test.Parser.SpeechParts.Atomics.Utils (checkLexeme)
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = describe "Adjectives Arbitrary" do
+  prop "Objective" $ checkLexeme adjectives
--- backend/test/UnitMain.hs
+++ backend/test/UnitMain.hs
@@ -1,12 +1,18 @@
 module Main where
 
-import           Test.Hspec                            (hspec, runIO)
+import           Test.Hspec                                 (hspec, runIO)
+import qualified Test.Lexer
+import qualified Test.Parser.SpeechParts.Atomics.Adjectives
 import qualified Test.Parser.SpeechParts.Atomics.Nouns
 import qualified Test.Parser.SpeechParts.Atomics.Verbs
 
 main :: IO ()
 main = hspec $ do
-  runIO $ putStrLn "Running unit tests..."
+  runIO $ putStrLn "Running property tests..."
   Test.Parser.SpeechParts.Atomics.Verbs.spec
   Test.Parser.SpeechParts.Atomics.Nouns.spec
+  Test.Parser.SpeechParts.Atomics.Adjectives.spec
+  runIO $ putStrLn "Running unit tests..."
+  Test.Lexer.spec
+
   pure ()
