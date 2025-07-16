951a8ddc4818c6cef97aceebbb8263b08f484233
Michael Litchard <michael.litchard@platonic.systems>
2025-07-16 19:37:02 +0000

    wip

--- backend/src/Evaluators/Location/General.hs
+++ backend/src/Evaluators/Location/General.hs
@@ -1,4 +1,5 @@
 module Evaluators.Location.General where
+import           Control.Monad.IO.Class        (MonadIO (liftIO))
 import qualified Data.Map.Strict
 import           Data.Text                     (Text)
 import           Error                         (throwLeftM, throwMaybeM)
@@ -6,34 +7,45 @@ import           GameState                     (getActionF, liftGS)
 import           Model.GameState               (ActionF (ComputeAction),
                                                 GameStateExceptT,
                                                 Location (Location),
-                                                ResolutionT)
+                                                ProcessSentence (ImplicitStimulusF),
+                                                ResolutionT (ResolutionT))
 import           Model.Parser                  (Sentence (Imperative))
 import           Model.Parser.Atomics.Verbs    (ImplicitStimulusVerb)
 import           Model.Parser.Composites.Verbs (Imperative (StimulusVerbPhrase),
                                                 StimulusVerbPhrase (ImplicitStimulusVerb))
 import           Model.Parser.GCase            (VerbKey (ImplicitStimulusKey))
 
-eval :: Sentence -> GameStateExceptT (Location -> ResolutionT ())
+eval :: Sentence -> GameStateExceptT (Location -> Either (ResolutionT ()) (ResolutionT ()))
+
 eval (Imperative imperative) = evalImperative imperative
 
-evalImperative :: Imperative -> GameStateExceptT (Location -> ResolutionT ())
+evalImperative :: Imperative -> GameStateExceptT (Location -> Either (ResolutionT ()) (ResolutionT ()))
+
+
 evalImperative (StimulusVerbPhrase stimulusVerbPhrase) =
   evalStimulusVerbPhrase stimulusVerbPhrase
 
 evalStimulusVerbPhrase :: StimulusVerbPhrase
-                            -> GameStateExceptT (Location -> ResolutionT ())
+                            -> GameStateExceptT (Location -> Either (ResolutionT ()) (ResolutionT ()))
+
+
 evalStimulusVerbPhrase (ImplicitStimulusVerb isv) = evalImplicitStimulusVerb isv
 
 evalImplicitStimulusVerb :: ImplicitStimulusVerb
-                              -> GameStateExceptT (Location -> ResolutionT ())
+                              -> GameStateExceptT (Location -> Either (ResolutionT ()) (ResolutionT ()))
 evalImplicitStimulusVerb isv = pure $
   \(Location desc _ amap) -> do
-      aid <- throwMaybeM errMsg $ Data.Map.Strict.lookup verbKey amap
-      actionF <- liftGS $ getActionF aid
-      case actionF of
-        ImplicitStimulusF res -> do
-          f <- throwLeftM caseMismatch res
-          f desc
+      case Data.Map.Strict.lookup verbKey amap of
+        Nothing -> Left $ ResolutionT $ liftIO $ print errMsg
+        Just aid -> do
+          actionF :: ActionF <- getActionF aid
+          Left $ ResolutionT $ liftIO $ print "placeholder until I figure it out"
+  {-
+          case actionF of
+            (ComputeAction (ImplicitStimulusF res)) -> do
+              f <- throwLeftM caseMismatch res
+              Right $ f desc
+              -}
   where
     verbKey :: VerbKey
     verbKey = ImplicitStimulusKey isv
