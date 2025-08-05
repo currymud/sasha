10b8766299ff0458ec14a59f9db21ab795210cd0
Michael Litchard <michael.litchard@platonic.systems>
2025-08-05 20:35:26 +0000

    wip

--- backend/src/Build/BedPuzzle/Actions/Get.hs
+++ backend/src/Build/BedPuzzle/Actions/Get.hs
@@ -1,37 +1,23 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use mapM_" #-}
 module Build.BedPuzzle.Actions.Get (get,getDenied) where
-import           Control.Monad.Identity          (Identity)
-import           Control.Monad.Reader            (asks)
+import           Control.Monad.Identity        (Identity)
+import           Control.Monad.Reader          (asks)
 import qualified Data.Map.Strict
-import           Data.Maybe                      (catMaybes)
-import           Data.Set                        (Set, elemAt, fromList, null,
-                                                  toList)
-import           Data.Text                       (Text)
-import           GameState                       (getLocationM, modifyLocationM,
-                                                  modifyNarration,
-                                                  modifyObjectActionManagementM,
-                                                  parseAcquisitionPhrase,
-                                                  updatePerceptionMapM, youSeeM)
-import           Model.GameState                 (AcquisitionActionF (AcquisitionActionF),
-                                                  ActionEffectKey (LocationKey, ObjectKey),
-                                                  ActionEffectMap (ActionEffectMap),
-                                                  ActionManagement (_acquisitionActionManagement, _directionalStimulusActionManagement, _implicitStimulusActionManagement, _somaticStimulusActionManagement),
-                                                  ActionMaps (ActionMaps, _acquisitionActionMap),
-                                                  Config (Config, _actionMaps),
-                                                  Effect (AcquisitionEffect, DirectionalStimulusEffect, ImplicitStimulusEffect, SomaticAccessEffect),
-                                                  GameComputation,
-                                                  Location (_locationActionManagement, _objectSemanticMap),
-                                                  Object,
-                                                  updateActionConsequence)
-import           Model.GID                       (GID)
-import           Model.Parser.Atomics.Adjectives (Adjective)
-import           Model.Parser.Atomics.Nouns      (Objective (Objective))
-import           Model.Parser.Composites.Nouns   (NounPhrase (DescriptiveNounPhrase, DescriptiveNounPhraseDet, NounPhrase, SimpleNounPhrase),
-                                                  ObjectPhrase (ObjectPhrase))
-import           Model.Parser.Composites.Verbs   (AcquisitionVerbPhrase (AcquisitionVerbPhrase))
-import           Model.Parser.GCase              (NounKey (ObjectiveKey))
-
+import           Data.Set                      (elemAt, null)
+import           Data.Text                     (Text)
+import           GameState                     (getObjectM, modifyNarration,
+                                                parseAcquisitionPhrase)
+import           Model.GameState               (AcquisitionActionF (AcquiredFromF, AcquisitionActionF, RemovedFromF),
+                                                ActionEffectMap (ActionEffectMap),
+                                                ActionManagement (_acquisitionActionManagement, _directionalStimulusActionManagement, _implicitStimulusActionManagement, _somaticStimulusActionManagement),
+                                                ActionMaps (_acquisitionActionMap),
+                                                Config (_actionMaps),
+                                                GameComputation,
+                                                Location (_locationActionManagement, _objectSemanticMap),
+                                                Object (_objectActionManagement),
+                                                updateActionConsequence)
+import           Model.Parser.Composites.Verbs (AcquisitionVerbPhrase (AcquisitionVerbPhrase))
 
 getDenied :: AcquisitionActionF
 getDenied = AcquisitionActionF (const (const (const denied)))
@@ -46,21 +32,20 @@ get = AcquisitionActionF getit
   where
     getit :: Location -> ActionEffectMap -> AcquisitionVerbPhrase -> GameComputation Identity ()
     getit loc actionEffectMap avp = do
-      let (objectPhrase,nounKey) = parseAcquisitionPhrase avp
-
-      -- Find the object in the current location
-      case Data.Map.Strict.lookup nounKey loc._objectSemanticMap of
-        Just objSet | not (Data.Set.null objSet) -> do
-          let oid = Data.Set.elemAt 0 objSet
-          pure ()
-        _ -> modifyNarration $ updateActionConsequence "You don't see that here."
-
+      executeLocationGet loc avp >>= \case
+        Left locationAction -> locationAction  -- Location handles it, don't check object
+        Right locationSuccess -> do
+          -- Location allows acquisition, now check object
+          executeObjectGet loc actionEffectMap avp >>= \case
+            Left objectAction -> objectAction
+            Right objectSuccess -> do
+              locationSuccess
+              objectSuccess
 
 executeLocationGet :: Location
                         -> AcquisitionVerbPhrase
                         -> GameComputation Identity (Either (GameComputation Identity ()) (GameComputation Identity ()))
 executeLocationGet loc avp = do
-
   -- Look up location's acquisition actions
   let locationAcquisitionActions = _acquisitionActionManagement (_locationActionManagement loc)
 
@@ -72,5 +57,47 @@ executeLocationGet loc avp = do
         Just (AcquisitionActionF locationAction) -> do
           -- Execute location action (removes from location)
           pure $ Left $ locationAction loc (ActionEffectMap mempty) avp
-        _ -> pure $ Left $ modifyNarration $ updateActionConsequence  "-- No location-specific action"
-    Nothing -> pure $ Left $ modifyNarration $ updateActionConsequence "-- No location-specific action for this acquisition"
+        Just (RemovedFromF locationRemovedAction) -> do
+          -- Execute location removed-from action
+          locationRemovedAction loc avp
+        Just (AcquiredFromF locationAcquiredAction) -> do
+          -- Execute location acquired-from action
+          locationAcquiredAction loc avp
+        _ -> pure $ Left $ modifyNarration $ updateActionConsequence "No location-specific action"
+    Nothing -> pure $ Right $ modifyNarration $ updateActionConsequence "Location allows general acquisition"
+
+executeObjectGet :: Location
+                 -> ActionEffectMap
+                 -> AcquisitionVerbPhrase
+                 -> GameComputation Identity (Either (GameComputation Identity ()) (GameComputation Identity ()))
+executeObjectGet loc actionEffectMap avp = do
+  let (objectPhrase, nounKey) = parseAcquisitionPhrase avp
+
+  -- Find the object in the current location
+  case Data.Map.Strict.lookup nounKey loc._objectSemanticMap of
+    Just objSet | not (Data.Set.null objSet) -> do
+      let oid = Data.Set.elemAt 0 objSet
+      obj <- getObjectM oid
+
+      -- Check if object has acquisition actions
+      let objectAcquisitionActions = _acquisitionActionManagement (_objectActionManagement obj)
+
+      case Data.Map.Strict.lookup avp objectAcquisitionActions of
+        Just objectActionGID -> do
+          -- Get the object action from the action map
+          actionMap <- asks (_acquisitionActionMap . _actionMaps)
+          case Data.Map.Strict.lookup objectActionGID actionMap of
+            Just (AcquisitionActionF objectAction) -> do
+              -- Execute object action
+              pure $ Left $ objectAction loc actionEffectMap avp
+            Just (AcquiredFromF objectAcquiredAction) -> do
+              -- Execute acquired-from action
+              objectAcquiredAction loc avp
+            Just (RemovedFromF objectRemovedAction) -> do
+              -- Execute removed-from action
+              objectRemovedAction loc avp
+            Nothing -> pure $ Left $ modifyNarration $ updateActionConsequence "Object action not found in action map"
+        Nothing ->
+          -- No object-specific action, allow general acquisition
+          pure $ Right $ modifyNarration $ updateActionConsequence "Object allows general acquisition"
+    _ -> pure $ Left $ modifyNarration $ updateActionConsequence "You don't see that here."
