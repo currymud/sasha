e612b9ecd565d8cf2820a5bc47a0100af5fd74fc
Michael Litchard <michael.litchard@platonic.systems>
2025-08-07 19:39:41 +0000

    wip

--- backend/src/Build/BedPuzzle/Actions/Objects/Robe.hs
+++ backend/src/Build/BedPuzzle/Actions/Objects/Robe.hs
@@ -1,26 +1,29 @@
 module Build.BedPuzzle.Actions.Objects.Robe where
-import           Build.Identifiers.Actions                               (whatChairGID)
+import           Build.Identifiers.Actions                               (getRobeGID,
+                                                                          whatRobeGID)
 import           Data.Map.Strict                                         (Map)
 import qualified Data.Map.Strict
 import qualified Data.Set
 import           Grammar.Parser.Partitions.Misc                          (the)
 import           Grammar.Parser.Partitions.Nouns.DirectionalStimulus     (table)
+import           Grammar.Parser.Partitions.Nouns.Objectives              (robe)
+import           Grammar.Parser.Partitions.Verbs.AcquisitionVerbs        (get)
 import           Grammar.Parser.Partitions.Verbs.DirectionalStimulusVerb (look)
 import           Model.GameState                                         (AcquisitionActionF,
                                                                           ActionManagement (ActionManagement),
                                                                           DirectionalStimulusActionF,
                                                                           ImplicitStimulusActionF,
                                                                           Object (Object, _description, _descriptives, _objectActionManagement, _shortName),
-                                                                          SomaticAccessActionF,
-                                                                          SpatialRelationship (ContainedIn))
+                                                                          SomaticAccessActionF)
 import           Model.GID                                               (GID)
-import           Model.Parser.Atomics.Verbs                              (AcquisitionVerb,
-                                                                          DirectionalStimulusVerb,
+import           Model.Parser.Atomics.Nouns                              (Objective)
+import           Model.Parser.Atomics.Verbs                              (DirectionalStimulusVerb,
                                                                           ImplicitStimulusVerb,
                                                                           SomaticAccessVerb)
 import           Model.Parser.Composites.Nouns                           (DirectionalStimulusNounPhrase (DirectionalStimulusNounPhrase),
-                                                                          NounPhrase (NounPhrase, SimpleNounPhrase))
-import           Model.Parser.Composites.Verbs                           (AcquisitionVerbPhrase)
+                                                                          NounPhrase (NounPhrase, SimpleNounPhrase),
+                                                                          ObjectPhrase (ObjectPhrase))
+import           Model.Parser.Composites.Verbs                           (AcquisitionVerbPhrase (SimpleAcquisitionVerbPhrase))
 
 robeObj :: Object
 robeObj =
@@ -39,8 +42,19 @@ robeObj =
     implicitStimulusVerbs :: Map ImplicitStimulusVerb (GID ImplicitStimulusActionF)
     implicitStimulusVerbs = Data.Map.Strict.empty
     directionalStimulusVerbs :: Map DirectionalStimulusVerb (GID DirectionalStimulusActionF)
-    directionalStimulusVerbs =  Data.Map.Strict.fromList [(look, whatChairGID :: GID DirectionalStimulusActionF)]
+    directionalStimulusVerbs =  Data.Map.Strict.fromList [(look, whatRobeGID :: GID DirectionalStimulusActionF)]
     somaticAccessVerbs :: Map SomaticAccessVerb (GID SomaticAccessActionF)
     somaticAccessVerbs = Data.Map.Strict.empty
     acquisitionVerbs :: Map AcquisitionVerbPhrase (GID AcquisitionActionF)
-    acquisitionVerbs = Data.Map.Strict.empty
+    acquisitionVerbs = Data.Map.Strict.fromList [(getRobeAVP, getRobeGID)]
+
+
+-- Add helper definitions
+getRobeAVP :: AcquisitionVerbPhrase
+getRobeAVP = SimpleAcquisitionVerbPhrase get robeObjective
+
+robeObjective :: ObjectPhrase
+robeObjective = ObjectPhrase robeNP
+
+robeNP :: NounPhrase Objective
+robeNP = SimpleNounPhrase robe
