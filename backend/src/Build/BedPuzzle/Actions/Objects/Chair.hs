1fd808531aa30da78074651f1cd1fa661f7cee68
Michael Litchard <michael.litchard@platonic.systems>
2025-08-10 20:33:06 +0000

    wip

--- backend/src/Build/BedPuzzle/Actions/Objects/Chair.hs
+++ backend/src/Build/BedPuzzle/Actions/Objects/Chair.hs
@@ -1,39 +1,21 @@
 module Build.BedPuzzle.Actions.Objects.Chair where
 import           Build.Identifiers.Actions                               (whatChairGID)
-import           Data.Map.Strict                                         (Map)
-import qualified Data.Map.Strict
 import qualified Data.Set
-import           Grammar.Parser.Partitions.Adjectives                    (small,
-                                                                          white)
+import           Grammar.Parser.Partitions.Adjectives                    (small)
 import           Grammar.Parser.Partitions.Misc                          (the)
-import           Grammar.Parser.Partitions.Nouns.DirectionalStimulus     (pill,
-                                                                          table)
+import           Grammar.Parser.Partitions.Nouns.DirectionalStimulus     (chair)
 import           Grammar.Parser.Partitions.Verbs.DirectionalStimulusVerb (look)
-import           Model.GameState                                         (AcquisitionActionF,
-                                                                          ActionManagement (DSAManagementKey),
+import           Model.GameState                                         (ActionManagement (DSAManagementKey),
                                                                           ActionManagementFunctions (ActionManagementFunctions),
-                                                                          ConsumptionActionF,
-                                                                          DirectionalStimulusActionF,
-                                                                          ImplicitStimulusActionF,
-                                                                          Object (Object, _description, _descriptives, _objectActionManagement, _shortName),
-                                                                          SomaticAccessActionF,
-                                                                          SpatialRelationship (Contains),
-                                                                          World (_spatialRelationshipMap))
-import           Model.GID                                               (GID)
-import           Model.Parser.Atomics.Verbs                              (AcquisitionVerb (AcquisitionVerb),
-                                                                          DirectionalStimulusVerb,
-                                                                          ImplicitStimulusVerb,
-                                                                          SomaticAccessVerb)
+                                                                          Object (Object, _description, _descriptives, _objectActionManagement, _shortName))
 import           Model.Parser.Composites.Nouns                           (DirectionalStimulusNounPhrase (DirectionalStimulusNounPhrase),
                                                                           NounPhrase (DescriptiveNounPhraseDet, SimpleNounPhrase))
-import           Model.Parser.Composites.Verbs                           (AcquisitionVerbPhrase,
-                                                                          ConsumptionVerbPhrase)
 
 chairObj :: Object
 chairObj =
   let
-  longDescription = DirectionalStimulusNounPhrase (DescriptiveNounPhraseDet the small table)
-  shortDescription = DirectionalStimulusNounPhrase (SimpleNounPhrase table)
+  longDescription = DirectionalStimulusNounPhrase (DescriptiveNounPhraseDet the small chair)
+  shortDescription = DirectionalStimulusNounPhrase (SimpleNounPhrase chair)
   in Object
        { _shortName = "a chair"
        , _description = "It's the chair next to your bed."
