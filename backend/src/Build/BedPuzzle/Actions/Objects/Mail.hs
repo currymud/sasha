24ead0d4a28c9489734ab1f8cf8dd4bdff2bcbe2
Michael Litchard <michael.litchard@platonic.systems>
2025-08-12 18:21:19 +0000

    wip

--- backend/src/Build/BedPuzzle/Actions/Objects/Mail.hs
+++ backend/src/Build/BedPuzzle/Actions/Objects/Mail.hs
@@ -1,20 +1,27 @@
 module Build.BedPuzzle.Actions.Objects.Mail where
-import           Build.Identifiers.Actions                               (notEvenMailGID)
+import           Build.Identifiers.Actions                               (getMailDeniedGID,
+                                                                          notEvenMailGID)
 import qualified Data.Set
 import           Grammar.Parser.Partitions.Misc                          (the)
 import           Grammar.Parser.Partitions.Nouns.DirectionalStimulus     (mail)
+import           Grammar.Parser.Partitions.Nouns.Objectives              (mail)
+import           Grammar.Parser.Partitions.Verbs.AcquisitionVerbs        (get)
 import           Grammar.Parser.Partitions.Verbs.DirectionalStimulusVerb (look)
-import           Model.GameState                                         (ActionManagement (DSAManagementKey),
+import           Model.GameState                                         (ActionManagement (AAManagementKey, DSAManagementKey),
                                                                           ActionManagementFunctions (ActionManagementFunctions),
                                                                           Object (Object, _description, _descriptives, _objectActionManagement, _shortName))
+import           Model.Parser.Atomics.Nouns                              (DirectionalStimulus,
+                                                                          Objective)
 import           Model.Parser.Composites.Nouns                           (DirectionalStimulusNounPhrase (DirectionalStimulusNounPhrase),
-                                                                          NounPhrase (NounPhrase, SimpleNounPhrase))
+                                                                          NounPhrase (NounPhrase, SimpleNounPhrase),
+                                                                          ObjectPhrase (ObjectPhrase))
+import           Model.Parser.Composites.Verbs                           (AcquisitionVerbPhrase (SimpleAcquisitionVerbPhrase))
 
 mailObj :: Object
 mailObj =
   let
-  longDescription = DirectionalStimulusNounPhrase (NounPhrase the mail)
-  shortDescription = DirectionalStimulusNounPhrase (SimpleNounPhrase mail)
+  longDescription = DirectionalStimulusNounPhrase (NounPhrase the dsMailDirectional)
+  shortDescription = DirectionalStimulusNounPhrase (SimpleNounPhrase dsMailDirectional)
   in Object
        { _shortName = "junk mail"
        , _description = "A pile of junk mail scattered on the table."
@@ -23,8 +30,22 @@ mailObj =
        }
   where
     verbMaps :: ActionManagementFunctions
-    verbMaps = directionalStimulusVerbs
+    verbMaps = ActionManagementFunctions $ Data.Set.fromList
+      [ DSAManagementKey look notEvenMailGID
+      , AAManagementKey getMailAVP getMailDeniedGID
+      ]
 
-    directionalStimulusVerbs :: ActionManagementFunctions
-    directionalStimulusVerbs = ActionManagementFunctions
-                                 $ Data.Set.singleton (DSAManagementKey look notEvenMailGID)
+mailObjective :: Objective
+mailObjective = Grammar.Parser.Partitions.Nouns.Objectives.mail
+
+dsMailDirectional :: DirectionalStimulus
+dsMailDirectional = Grammar.Parser.Partitions.Nouns.DirectionalStimulus.mail
+
+mailObjectPhrase :: ObjectPhrase
+mailObjectPhrase = ObjectPhrase mailNP
+
+getMailAVP :: AcquisitionVerbPhrase
+getMailAVP = SimpleAcquisitionVerbPhrase get mailObjectPhrase
+
+mailNP :: NounPhrase Objective
+mailNP = SimpleNounPhrase mailObjective
