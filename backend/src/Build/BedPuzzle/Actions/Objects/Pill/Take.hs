d9db4df5a02eda382d1ab80fb19e70a9196def7a
Michael Litchard <michael.litchard@platonic.systems>
2025-08-12 00:11:24 +0000

    wip

--- backend/src/Build/BedPuzzle/Actions/Objects/Pill/Take.hs
+++ backend/src/Build/BedPuzzle/Actions/Objects/Pill/Take.hs
@@ -1,13 +1,24 @@
 module Build.BedPuzzle.Actions.Objects.Pill.Take (alreadyTookPillF, pillTooFarF, takePillF, takePillDeniedF) where
 
 import           Control.Monad.Identity        (Identity)
+import           Control.Monad.State           (modify')
+import qualified Data.Map.Strict
+import           Data.Set                      (Set)
 import qualified Data.Set
 import           Data.Text                     (Text)
 import           GameState                     (modifyNarration,
+                                                modifyObjectActionManagementM,
                                                 modifySpatialRelationshipsForObjectM)
-import           Model.GameState               (ActionEffectMap,
+import           Model.GameState               (ActionEffectKey (ObjectKey, PlayerKey),
+                                                ActionEffectMap (ActionEffectMap),
+                                                ActionManagement (DSAManagementKey, NPManagementKey, PPManagementKey),
+                                                ActionManagementFunctions (ActionManagementFunctions),
                                                 ConsumptionActionF (ConsumptionActionF),
-                                                GameComputation, Object,
+                                                Effect (DirectionalStimulusEffect, NegativePosturalEffect, PositivePosturalEffect),
+                                                GameComputation,
+                                                GameState (_player), Object,
+                                                Player (_playerActions),
+                                                PlayerKey (PlayerKeyObject),
                                                 SpatialRelationship (ContainedIn, SupportedBy),
                                                 updateActionConsequence)
 import           Model.GID                     (GID)
@@ -42,13 +53,58 @@ takePillF :: ConsumptionActionF
 takePillF = ConsumptionActionF takeIt
   where
     takeIt :: GID Object -> ActionEffectMap -> ConsumptionVerbPhrase -> GameComputation Identity ()
-    takeIt objectGID _actionEffectMap _cvp = do
+    takeIt objectGID actionEffectMap cvp = do
       -- Remove the pill from whatever is containing/supporting it
       modifySpatialRelationshipsForObjectM objectGID $ \relationships ->
         Data.Set.filter (not . isLocationRelationship) relationships
 
       -- Fire the consumption consequence
       modifyNarration $ updateActionConsequence "You take the pill and swallow it."
+
+      -- Process effects
+      let ActionEffectMap effectMap = actionEffectMap
+      mapM_ (processEffectEntry cvp) (Data.Map.Strict.toList effectMap)
+
+    processEffectEntry :: ConsumptionVerbPhrase -> (ActionEffectKey, Set Effect) -> GameComputation Identity ()
+    processEffectEntry cvp (effectKey, effects) = do
+      mapM_ (processEffect cvp effectKey) (Data.Set.toList effects)
+
+    processEffect :: ConsumptionVerbPhrase -> ActionEffectKey -> Effect -> GameComputation Identity ()
+    processEffect cvp (PlayerKey (PlayerKeyObject oid)) (PositivePosturalEffect verb newActionGID) = do
+      -- Update the player's positive postural actions
+      modify' $ \gs ->
+        let player = gs._player
+            ActionManagementFunctions playerActionSet = _playerActions player
+            -- Remove any existing positive postural action for this verb
+            filteredActions = Data.Set.filter (\case PPManagementKey v _ -> v /= verb; _ -> True) playerActionSet
+            -- Add the new action
+            updatedActions = Data.Set.insert (PPManagementKey verb newActionGID) filteredActions
+            updatedPlayerActions = ActionManagementFunctions updatedActions
+            updatedPlayer = player { _playerActions = updatedPlayerActions }
+        in gs { _player = updatedPlayer }
+
+    processEffect cvp (PlayerKey (PlayerKeyObject oid)) (NegativePosturalEffect verb newActionGID) = do
+      -- Update the player's negative postural actions
+      modify' $ \gs ->
+        let player = gs._player
+            ActionManagementFunctions playerActionSet = _playerActions player
+            -- Remove any existing negative postural action for this verb
+            filteredActions = Data.Set.filter (\case NPManagementKey v _ -> v /= verb; _ -> True) playerActionSet
+            -- Add the new action
+            updatedActions = Data.Set.insert (NPManagementKey verb newActionGID) filteredActions
+            updatedPlayerActions = ActionManagementFunctions updatedActions
+            updatedPlayer = player { _playerActions = updatedPlayerActions }
+        in gs { _player = updatedPlayer }
+
+    processEffect _ (ObjectKey oid) (DirectionalStimulusEffect verb newActionGID) = do
+      -- Update the object's directional stimulus action
+      modifyObjectActionManagementM oid $ \actionMgmt ->
+        let ActionManagementFunctions actionSet = actionMgmt
+            filteredActions = Data.Set.filter (\case DSAManagementKey v _ -> v /= verb; _ -> True) actionSet
+            updatedActions = Data.Set.insert (DSAManagementKey verb newActionGID) filteredActions
+        in ActionManagementFunctions updatedActions
+
+    processEffect _ _ _ = pure () -- Handle other effect types as needed
 
     isLocationRelationship :: SpatialRelationship -> Bool
     isLocationRelationship (ContainedIn _) = True
