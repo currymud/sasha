57463854e6d081e3636ed3567cc521a8c3d81d13
Michael Litchard <michael.litchard@platonic.systems>
2025-08-16 22:56:55 +0000

    wip

---
+++ backend/src/Build/GameStateGeneration/LocationSpec/Locations.hs
@@ -0,0 +1,45 @@
+module Build.GameStateGeneration.LocationSpec.Locations where
+
+import           Build.GameStateGeneration.LocationSpec.LocationGIDs  (bedroomGID,
+                                                                       bedroomUpstairsGID)
+import           Build.GameStateGeneration.ObjectSpec                 (defaultLocation,
+                                                                       withLocationBehaviors,
+                                                                       withObjects,
+                                                                       withTitle)
+import           Build.GameStateGeneration.ObjectSpec.ObjectGIDS      (chairGID,
+                                                                       smallTableGID)
+import           Build.Identifiers.Actions                            (pitchBlackFGID)
+import           Build.Templates.Identification                       (makeLocationMap)
+import           Data.Function                                        ((&))
+import           Grammar.Parser.Partitions.Nouns.DirectionalStimulus  (chair,
+                                                                       table)
+import qualified Grammar.Parser.Partitions.Verbs.ImplicitStimulusVerb (look)
+import           Model.GameState                                      (ActionManagement (ISAManagementKey),
+                                                                       Location)
+import           Model.Parser.GCase                                   (NounKey (DirectionalStimulusKey))
+
+-- Create locations using builder pattern
+bedroom :: Location
+bedroom = defaultLocation
+  & withTitle "Bedroom in Bed"
+  & withObjects [ (chairGID, DirectionalStimulusKey chair)
+                , (smallTableGID, DirectionalStimulusKey table)
+                ]
+  & withLocationBehaviors [ISAManagementKey Grammar.Parser.Partitions.Verbs.ImplicitStimulusVerb.look pitchBlackFGID]
+bedroomUpstairs :: Location
+bedroomUpstairs = defaultLocation
+  & withTitle "Upstairs Bedroom"
+  & withObjects [ (chairGID, DirectionalStimulusKey chair) ]
+  & withLocationBehaviors [ISAManagementKey Grammar.Parser.Partitions.Verbs.ImplicitStimulusVerb.look pitchBlackFGID]
+
+-- Generate the location map using Template Haskell
+makeLocationMap [ ('bedroomGID, 'bedroom)
+                , ('bedroomUpstairsGID, 'bedroomUpstairs)
+                ]
+
+-- This generates:
+-- locationMap :: GIDToDataMap Location Location
+-- locationMap = GIDToDataMap $ Data.Map.Strict.fromList
+--   [ (bedroomGID, bedroom)
+--   , (bedroomUpstairsGID, bedroomUpstairs)
+--   ]
