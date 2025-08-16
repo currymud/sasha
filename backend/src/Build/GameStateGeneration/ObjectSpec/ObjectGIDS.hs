3e7e99a1e36111752cd7d0ce2ab66df30a0b76e0
Michael Litchard <michael.litchard@platonic.systems>
2025-08-16 23:33:52 +0000

    wip

--- backend/src/Build/GameStateGeneration/ObjectSpec/ObjectGIDS.hs
+++ backend/src/Build/GameStateGeneration/ObjectSpec/ObjectGIDS.hs
@@ -3,16 +3,24 @@ import           Build.Templates.Identification                      (declareObj
 import           Grammar.Parser.Partitions.Adjectives                (small)
 import           Grammar.Parser.Partitions.Misc                      (the)
 import           Grammar.Parser.Partitions.Nouns.DirectionalStimulus (chair,
+                                                                      floor,
+                                                                      mail,
                                                                       pill,
+                                                                      pocket,
+                                                                      robe,
                                                                       table)
 import           Model.Parser.Atomics.Nouns                          (DirectionalStimulus)
 import           Model.Parser.Composites.Nouns
-
+import           Prelude                                             hiding
+                                                                     (floor)
 
 declareObjectGIDs [ (SimpleNounPhrase chair, Nothing)                      -- chairGID
                   , (DescriptiveNounPhraseDet the small table, Nothing)    -- smallTableGID
-                  , (DescriptiveNounPhraseDet the small table, Just "bedroom")  -- smallTableBedroomGID
-                  , (DescriptiveNounPhraseDet the small table, Just "kitchen")  -- smallTableKitchenGID
+                  , (SimpleNounPhrase pill, Nothing)                       -- pillGID
+                  , (SimpleNounPhrase mail, Nothing)                       -- mailGID
+                  , (SimpleNounPhrase robe, Nothing)                       -- robeGID
+                  , (SimpleNounPhrase pocket, Nothing)                     -- pocketGID
+                  , (SimpleNounPhrase floor, Nothing)                      -- floorGID
                   ]
 
 
--- backend/src/Build/GameStateGeneration/ObjectSpec/Objects.hs
+++ backend/src/Build/GameStateGeneration/ObjectSpec/Objects.hs
@@ -6,33 +6,107 @@ import           Build.GameStateGeneration.ObjectSpec                    (defaul
                                                                           withDescription,
                                                                           withDescriptives,
                                                                           withShortName)
-import           Build.GameStateGeneration.ObjectSpec.ObjectGIDS         (chairGID)
-import           Build.Identifiers.Actions                               (seeChairFGID,
-                                                                          seeTableGID)
+import           Build.GameStateGeneration.ObjectSpec.ObjectGIDS         (chairGID,
+                                                                          smallTableGID)
+import           Build.Identifiers.Actions                               (alreadyHaveRobeFGID,
+                                                                          seeChairFGID,
+                                                                          seeMailGID,
+                                                                          seeRobeChairGID,
+                                                                          seeTableGID,
+                                                                          takePillDeniedFGID,
+                                                                          whatPillGID)
 import           Build.Templates.Identification                          (makeObjectMap)
 import           Data.Function                                           ((&))
 import           Grammar.Parser.Partitions.Adjectives                    (small)
 import           Grammar.Parser.Partitions.Misc                          (the)
-import           Grammar.Parser.Partitions.Nouns.DirectionalStimulus     (chair,
+import qualified Grammar.Parser.Partitions.Nouns.DirectionalStimulus     (chair,
+                                                                          floor,
+                                                                          mail,
+                                                                          pill,
+                                                                          pocket,
+                                                                          robe,
                                                                           table)
-import qualified Grammar.Parser.Partitions.Verbs.DirectionalStimulusVerb (look)
-import           Model.GameState                                         (ActionManagement (DSAManagementKey),
+
+import           Build.BedPuzzle.Actions.Objects.Pill                    (takePillCVP)
+import           Build.BedPuzzle.Actions.Objects.Robe                    (getRobeAVP)
+import qualified Grammar.Parser.Partitions.Nouns.Consumables             (pill)
+import           Grammar.Parser.Partitions.Verbs.AcquisitionVerbs        (get)
+import           Grammar.Parser.Partitions.Verbs.DirectionalStimulusVerb (look)
+import           Model.GameState                                         (ActionManagement (AAManagementKey, CAManagementKey, DSAManagementKey),
                                                                           Object)
+import           Model.Parser.Atomics.Nouns                              (DirectionalStimulus)
 import           Model.Parser.Composites.Nouns                           (NounPhrase (DescriptiveNounPhraseDet, SimpleNounPhrase))
+
+
+chairDS :: DirectionalStimulus
+chairDS = Grammar.Parser.Partitions.Nouns.DirectionalStimulus.chair
 
 chairObj :: Object
 chairObj = defaultObject
   & withShortName "a chair"
   & withDescription "It's the chair next to your bed"
-  & withDescriptives [SimpleNounPhrase chair, DescriptiveNounPhraseDet the small chair]
+  & withDescriptives [SimpleNounPhrase chairDS, DescriptiveNounPhraseDet the small chairDS]
   & withBehaviors [DSAManagementKey Grammar.Parser.Partitions.Verbs.DirectionalStimulusVerb.look seeChairFGID]
 
+
+tableDS :: DirectionalStimulus
+tableDS = Grammar.Parser.Partitions.Nouns.DirectionalStimulus.table
 
 smallTableObj :: Object
 smallTableObj = defaultObject
   & withShortName "small table"
   & withDescription "A small bedside table"
-  & withDescriptives [DescriptiveNounPhraseDet the small table]
+  & withDescriptives [DescriptiveNounPhraseDet the small tableDS]
   & withBehaviors [DSAManagementKey Grammar.Parser.Partitions.Verbs.DirectionalStimulusVerb.look seeTableGID]
 
-makeObjectMap [ ('chairGID, 'chairObj) ]  -- objectMap :: GIDToDataMap Object Object
+pillDS :: DirectionalStimulus
+pillDS = Grammar.Parser.Partitions.Nouns.DirectionalStimulus.pill
+
+pillObj :: Object
+pillObj = defaultObject
+  & withShortName "pill"
+  & withDescription "A small, round pill. Probably good for headaches."
+  & withDescriptives [SimpleNounPhrase pillDS]
+  & withBehaviors [DSAManagementKey look whatPillGID, CAManagementKey takePillCVP takePillDeniedFGID]
+
+mailDS :: DirectionalStimulus
+mailDS = Grammar.Parser.Partitions.Nouns.DirectionalStimulus.mail
+
+mailObj :: Object
+mailObj = defaultObject
+  & withShortName "mail"
+  & withDescription "Some mail on the table"
+  & withDescriptives [SimpleNounPhrase mailDS]
+  & withBehaviors [DSAManagementKey look seeMailGID]
+
+
+robeDS :: DirectionalStimulus
+robeDS = Grammar.Parser.Partitions.Nouns.DirectionalStimulus.robe
+
+robeObj :: Object
+robeObj = defaultObject
+  & withShortName "robe"
+  & withDescription "A comfortable robe"
+  & withDescriptives [SimpleNounPhrase robeDS]
+  & withBehaviors [DSAManagementKey look seeRobeChairGID,
+                   AAManagementKey getRobeAVP alreadyHaveRobeFGID]  -- Get behavior
+
+pocketDS :: DirectionalStimulus
+pocketDS = Grammar.Parser.Partitions.Nouns.DirectionalStimulus.pocket
+
+pocketObj :: Object
+pocketObj = defaultObject
+  & withShortName "pocket"
+  & withDescription "A pocket in the robe"
+  & withDescriptives [SimpleNounPhrase pocketDS]
+
+floorDS :: DirectionalStimulus
+floorDS = Grammar.Parser.Partitions.Nouns.DirectionalStimulus.floor
+
+floorObj :: Object
+floorObj = defaultObject
+  & withShortName "floor"
+  & withDescription "The bedroom floor"
+  & withDescriptives [SimpleNounPhrase floorDS]
+
+makeObjectMap [ ('chairGID, 'chairObj), ('smallTableGID, 'smallTableObj) ]  -- objectMap :: GIDToDataMap Object Object
