1517357eed1152ff5e22a86ad6a75db758b01474
Michael Litchard <michael.litchard@platonic.systems>
2025-08-17 01:32:12 +0000

    wip

--- backend/src/Build/GameStateGeneration/LocationSpec/Locations.hs
+++ backend/src/Build/GameStateGeneration/LocationSpec/Locations.hs
@@ -16,7 +16,7 @@ import           Grammar.Parser.Partitions.Nouns.DirectionalStimulus  (chair,
 import qualified Grammar.Parser.Partitions.Verbs.ImplicitStimulusVerb (look)
 import           Model.GameState                                      (ActionManagement (ISAManagementKey),
                                                                        Location)
-import           Model.Parser.GCase                                   (NounKey (DirectionalStimulusKey))
+import           Model.Parser.GCase                                   (NounKey (DirectionalStimulusKey, ObjectiveKey))
 
 import           Build.GameStateGeneration.ObjectSpec.ObjectGIDS      (chairGID,
                                                                        floorGID,
@@ -28,6 +28,7 @@ import           Grammar.Parser.Partitions.Nouns.DirectionalStimulus  (chair,
                                                                        mail,
                                                                        robe,
                                                                        table)
+import qualified Grammar.Parser.Partitions.Nouns.Objectives           as Objectives
 import           Prelude                                              hiding
                                                                       (floor)
 -- Create locations using builder pattern
@@ -39,6 +40,11 @@ bedroom = defaultLocation
                 , (robeGID, DirectionalStimulusKey robe)
                 , (mailGID, DirectionalStimulusKey mail)
                 , (floorGID, DirectionalStimulusKey floor)
+                , (chairGID, ObjectiveKey Objectives.chair)
+                , (smallTableGID, ObjectiveKey Objectives.table)
+                , (robeGID, ObjectiveKey Objectives.robe)
+                , (mailGID, ObjectiveKey Objectives.mail)
+                , (floorGID, ObjectiveKey Objectives.floor)
                 ]
   & withLocationBehaviors [ISAManagementKey Grammar.Parser.Partitions.Verbs.ImplicitStimulusVerb.look pitchBlackFGID]
 bedroomUpstairs :: Location
