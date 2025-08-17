28b6e85349d4f668e29f9e6e3d172c6d0a3ac646
Michael Litchard <michael.litchard@platonic.systems>
2025-08-17 22:21:17 +0000

    wip

--- backend/src/Model/GameState/GameStateDSL.hs
+++ backend/src/Model/GameState/GameStateDSL.hs
@@ -1,46 +1,64 @@
 module Model.GameState.GameStateDSL where
 
 import           Data.Kind                     (Type)
-import           Model.GameState               (AcquisitionActionF, ActionKey,
+import           Model.GameState               (AcquisitionActionF,
+                                                ActionEffectMap, ActionKey,
                                                 ConsumptionActionF,
                                                 DirectionalStimulusActionF,
-                                                GameState,
+                                                Effect, GameState,
                                                 ImplicitStimulusActionF,
                                                 Location, Object, Player,
-                                                PosturalActionF,
+                                                PlayerKey, PosturalActionF,
                                                 SomaticAccessActionF,
                                                 SpatialRelationship)
 import           Model.GID                     (GID)
-import           Model.Parser.Atomics.Nouns    (DirectionalStimulus)
+import           Model.Parser.Atomics.Nouns    (Consumable, Container,
+                                                DirectionalStimulus, Objective)
+import           Model.Parser.Atomics.Verbs    (ConsumptionVerb,
+                                                DirectionalStimulusVerb,
+                                                ImplicitStimulusVerb,
+                                                PositivePosturalVerb)
 import           Model.Parser.Composites.Nouns (NounPhrase)
+import           Model.Parser.Composites.Verbs (AcquisitionVerbPhrase)
 
--- | World building DSL - designed to teach Functor -> Applicative -> Monad progression
+-- | World building DSL
 type WorldDSL :: Type -> Type
 data WorldDSL :: Type -> Type where
-  -- Pure values (teaches Pure/return)
+  -- Pure values
   Pure :: a -> WorldDSL a
 
-  -- Functor operations (teaches <$>)
+  -- Functor operations
   Map :: (a -> b) -> WorldDSL a -> WorldDSL b
 
-  -- Applicative operations (teaches <*> and sequential effects)
+  -- Applicative operations
   Apply :: WorldDSL (a -> b) -> WorldDSL a -> WorldDSL b
-  Sequence :: WorldDSL a -> WorldDSL b -> WorldDSL b  -- (*>)
+  Sequence :: WorldDSL a -> WorldDSL b -> WorldDSL b
 
-  -- Monadic operations (teaches >>= and context-dependent computation)
+  -- Monadic operations
   Bind :: WorldDSL a -> (a -> WorldDSL b) -> WorldDSL b
 
--- GID Declaration constructors - let students create and bind their own names
+  -- GID Declaration constructors
   DeclareObjectGID :: NounPhrase DirectionalStimulus -> WorldDSL (GID Object)
+  DeclareObjectiveGID :: NounPhrase Objective -> WorldDSL (GID Object)
+  DeclareConsumableGID :: NounPhrase Consumable -> WorldDSL (GID Object)
+  DeclareContainerGID :: NounPhrase Container -> WorldDSL (GID Object)
   DeclareLocationGID :: NounPhrase DirectionalStimulus -> WorldDSL (GID Location)
 
+  -- Initial value constructors
+  InitialObject :: Object -> WorldDSL Object
+  InitialLocation :: Location -> WorldDSL Location
+  InitialPlayer :: Player -> WorldDSL Player
 
-  -- Core world building primitives - following current build system pattern
-  BuildObject :: GID Object -> Object -> (Object -> Object) -> WorldDSL Object
-  BuildLocation :: GID Location -> Location -> (Location -> Location) -> WorldDSL Location
-  BuildPlayer :: Player -> (Player -> Player) -> WorldDSL Player
+  -- Builder constructors - compositional with WorldDSL parameters
+  BuildObject :: WorldDSL (GID Object) -> WorldDSL Object -> (Object -> Object) -> WorldDSL Object
+  BuildLocation :: WorldDSL (GID Location) -> WorldDSL Location -> (Location -> Location) -> WorldDSL Location
+  BuildPlayer :: WorldDSL Player -> (Player -> Player) -> WorldDSL Player
 
--- ActionKey construction - raw values are fine for constructors
+  -- Registration - returns GIDs for composition
+  RegisterObject :: WorldDSL (GID Object) -> WorldDSL Object -> WorldDSL (GID Object)
+  RegisterLocation :: WorldDSL (GID Location) -> WorldDSL Location -> WorldDSL (GID Location)
+
+  -- ActionKey construction
   CreateImplicitStimulusActionKey :: GID ImplicitStimulusActionF -> WorldDSL ActionKey
   CreateDirectionalStimulusActionKey :: GID DirectionalStimulusActionF -> WorldDSL ActionKey
   CreateSomaticAccessActionKey :: GID SomaticAccessActionF -> WorldDSL ActionKey
@@ -48,16 +66,24 @@ data WorldDSL :: Type -> Type where
   CreateConsumptionActionKey :: GID ConsumptionActionF -> WorldDSL ActionKey
   CreatePosturalActionKey :: GID PosturalActionF -> WorldDSL ActionKey
 
-  -- Register mappings - both single and collections
-  RegisterObject :: GID Object -> Object -> WorldDSL ()
-  RegisterObjects :: [(GID Object, Object)] -> WorldDSL ()
-  RegisterLocation :: GID Location -> Location -> WorldDSL ()
-  RegisterLocations :: [(GID Location, Location)] -> WorldDSL ()
+  -- Effect creation
+  CreateImplicitStimulusEffect :: WorldDSL ImplicitStimulusVerb -> WorldDSL (GID ImplicitStimulusActionF) -> WorldDSL Effect
+  CreateDirectionalStimulusEffect :: WorldDSL DirectionalStimulusVerb -> WorldDSL (GID DirectionalStimulusActionF) -> WorldDSL Effect
+  CreateAcquisitionEffect :: WorldDSL AcquisitionVerbPhrase -> WorldDSL (GID AcquisitionActionF) -> WorldDSL Effect
+  CreateConsumptionEffect :: WorldDSL ConsumptionVerb -> WorldDSL (GID Object) -> WorldDSL (GID ConsumptionActionF) -> WorldDSL Effect
+  CreatePosturalEffect :: WorldDSL PositivePosturalVerb -> WorldDSL (GID PosturalActionF) -> WorldDSL Effect
+  CreatePerceptionEffect :: WorldDSL Effect
 
-  -- Spatial relationships - single only, collections built from primitives
-  SetSpatial :: GID Object -> [SpatialRelationship] -> WorldDSL ()
+  -- Effect management
+  LinkEffectToObject :: WorldDSL (GID Object) -> WorldDSL Effect -> WorldDSL ()
+  LinkEffectToLocation :: WorldDSL (GID Location) -> WorldDSL Effect -> WorldDSL ()
+  LinkEffectToPlayer :: WorldDSL PlayerKey -> WorldDSL Effect -> WorldDSL ()
+  RegisterEffectRegistry :: WorldDSL ActionKey -> WorldDSL ActionEffectMap -> WorldDSL ()
 
-  -- Context queries that demonstrate why we need monads
+  -- Spatial relationships - compositional
+  SetSpatial :: WorldDSL (GID Object) -> WorldDSL [SpatialRelationship] -> WorldDSL ()
+
+  -- Context queries
   GetCurrentObjects :: WorldDSL [Object]
   GetCurrentLocations :: WorldDSL [Location]
   GetCurrentPlayer :: WorldDSL Player
@@ -65,7 +91,7 @@ data WorldDSL :: Type -> Type where
   -- Final assembly
   FinalizeGameState :: WorldDSL GameState
 
--- Instances that teach the hierarchy explicitly
+-- Instances
 instance Functor WorldDSL where
   fmap = Map
 
@@ -77,12 +103,107 @@ instance Monad WorldDSL where
    return = pure
    (>>=) = Bind
 
+-- Smart constructors
+declareObjectGID :: NounPhrase DirectionalStimulus -> WorldDSL (GID Object)
+declareObjectGID = DeclareObjectGID
+
+declareObjectiveGID :: NounPhrase Objective -> WorldDSL (GID Object)
+declareObjectiveGID = DeclareObjectiveGID
+
+declareConsumableGID :: NounPhrase Consumable -> WorldDSL (GID Object)
+declareConsumableGID = DeclareConsumableGID
+
+declareContainerGID :: NounPhrase Container -> WorldDSL (GID Object)
+declareContainerGID = DeclareContainerGID
+
+declareLocationGID :: NounPhrase DirectionalStimulus -> WorldDSL (GID Location)
+declareLocationGID = DeclareLocationGID
+
+initialObject :: Object -> WorldDSL Object
+initialObject = InitialObject
+
+initialLocation :: Location -> WorldDSL Location
+initialLocation = InitialLocation
+
+initialPlayer :: Player -> WorldDSL Player
+initialPlayer = InitialPlayer
+
+buildObject :: WorldDSL (GID Object) -> WorldDSL Object -> (Object -> Object) -> WorldDSL Object
+buildObject = BuildObject
+
+buildLocation :: WorldDSL (GID Location) -> WorldDSL Location -> (Location -> Location) -> WorldDSL Location
+buildLocation = BuildLocation
+
+buildPlayer :: WorldDSL Player -> (Player -> Player) -> WorldDSL Player
+buildPlayer = BuildPlayer
+
+registerObject :: WorldDSL (GID Object) -> WorldDSL Object -> WorldDSL (GID Object)
+registerObject = RegisterObject
+
+registerLocation :: WorldDSL (GID Location) -> WorldDSL Location -> WorldDSL (GID Location)
+registerLocation = RegisterLocation
+
+setSpatial :: WorldDSL (GID Object) -> WorldDSL [SpatialRelationship] -> WorldDSL ()
+setSpatial = SetSpatial
+
+createImplicitStimulusEffect :: WorldDSL ImplicitStimulusVerb -> WorldDSL (GID ImplicitStimulusActionF) -> WorldDSL Effect
+createImplicitStimulusEffect = CreateImplicitStimulusEffect
+
+createDirectionalStimulusEffect :: WorldDSL DirectionalStimulusVerb -> WorldDSL (GID DirectionalStimulusActionF) -> WorldDSL Effect
+createDirectionalStimulusEffect = CreateDirectionalStimulusEffect
+
+createAcquisitionEffect :: WorldDSL AcquisitionVerbPhrase -> WorldDSL (GID AcquisitionActionF) -> WorldDSL Effect
+createAcquisitionEffect = CreateAcquisitionEffect
+
+createConsumptionEffect :: WorldDSL ConsumptionVerb -> WorldDSL (GID Object) -> WorldDSL (GID ConsumptionActionF) -> WorldDSL Effect
+createConsumptionEffect = CreateConsumptionEffect
+
+createPosturalEffect :: WorldDSL PositivePosturalVerb -> WorldDSL (GID PosturalActionF) -> WorldDSL Effect
+createPosturalEffect = CreatePosturalEffect
+
+createPerceptionEffect :: WorldDSL Effect
+createPerceptionEffect = CreatePerceptionEffect
+
+linkEffectToObject :: WorldDSL (GID Object) -> WorldDSL Effect -> WorldDSL ()
+linkEffectToObject = LinkEffectToObject
+
+linkEffectToLocation :: WorldDSL (GID Location) -> WorldDSL Effect -> WorldDSL ()
+linkEffectToLocation = LinkEffectToLocation
+
+linkEffectToPlayer :: WorldDSL PlayerKey -> WorldDSL Effect -> WorldDSL ()
+linkEffectToPlayer = LinkEffectToPlayer
+
+registerEffectRegistry :: WorldDSL ActionKey -> WorldDSL ActionEffectMap -> WorldDSL ()
+registerEffectRegistry = RegisterEffectRegistry
+
+getCurrentObjects :: WorldDSL [Object]
+getCurrentObjects = GetCurrentObjects
+
+getCurrentLocations :: WorldDSL [Location]
+getCurrentLocations = GetCurrentLocations
+
+getCurrentPlayer :: WorldDSL Player
+getCurrentPlayer = GetCurrentPlayer
+
+finalizeGameState :: WorldDSL GameState
+finalizeGameState = FinalizeGameState
+
+-- Convenience functions
 buildSequentially :: WorldDSL a -> WorldDSL b -> WorldDSL b
 buildSequentially = Sequence
 
-setSpatial :: GID Object -> [SpatialRelationship] -> WorldDSL ()
-setSpatial = SetSpatial
+-- Batch operations built from primitives
+registerObjects :: WorldDSL [(GID Object, Object)] -> WorldDSL [GID Object]
+registerObjects objectsAction = do
+  objects <- objectsAction
+  mapM (\(gid, obj) -> registerObject (pure gid) (pure obj)) objects
 
--- Collection operations built from GADT primitives - demonstrates composition
-setSpatials :: [(GID Object, [SpatialRelationship])] -> WorldDSL ()
-setSpatials = foldr (buildSequentially . uncurry setSpatial) (pure ())
+registerLocations :: WorldDSL [(GID Location, Location)] -> WorldDSL [GID Location]
+registerLocations locationsAction = do
+  locations <- locationsAction
+  mapM (\(gid, loc) -> registerLocation (pure gid) (pure loc)) locations
+
+setSpatials :: WorldDSL [(GID Object, [SpatialRelationship])] -> WorldDSL ()
+setSpatials objectsAndRels = do
+  pairs <- objectsAndRels
+  sequence_ [setSpatial (pure objGID) (pure rels) | (objGID, rels) <- pairs]
