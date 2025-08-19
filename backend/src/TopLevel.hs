0727f79a3f4c9942c39e9f1c044069e7bd7682cc
Michael Litchard <michael.litchard@platonic.systems>
2025-08-19 17:16:10 +0000

    wip

--- backend/src/TopLevel.hs
+++ backend/src/TopLevel.hs
@@ -1,17 +1,20 @@
 module TopLevel where
-import           Control.Monad.Identity    (Identity (Identity))
+import           Control.Monad.Identity    (Identity)
 import           Control.Monad.IO.Class    (MonadIO (liftIO))
-import           Control.Monad.State.Class (gets)
+import           Control.Monad.State       (get)
+import           Control.Monad.State.Class (gets, put)
 import           Data.Kind                 (Type)
+import qualified Data.Map.Strict
 import           Data.Text                 (Text, empty, pack)
 import           GameState                 (clearNarration, modifyNarration)
 import           Grammar.Parser            (parseTokens)
 import           Grammar.Parser.Lexer      (Lexeme, lexify, tokens)
 import           Model.GameState           (DisplayT, GameComputation,
-                                            GameState (_evaluation, _narration),
-                                            GameT, _actionConsequence,
-                                            _playerAction, liftDisplay,
-                                            transformToIO,
+                                            GameState (_evaluation, _narration, _systemEffectRegistry),
+                                            GameT,
+                                            SystemEffect (PerceptionSystemEffect),
+                                            _actionConsequence, _playerAction,
+                                            liftDisplay, transformToIO,
                                             updateActionConsequence)
 import           Model.Parser              (Sentence)
 import           Relude.String.Conversion  (ToText (toText))
@@ -42,9 +45,16 @@ runGame comp' = do
   attSentence <- trySentence <$> liftIO getInput
   case attSentence of
     Left err       -> runGame $ errorHandler err
-    Right sentence ->runGame $ toGameComputation sentence
+    Right sentence ->runGame $ processWithSystemEffects sentence
 
-
+processWithSystemEffects :: Sentence -> GameComputation Identity ()
+processWithSystemEffects sentence = do
+  gameState <- get
+  let systemEffectComputations =  (\(PerceptionSystemEffect computation) -> computation) `fmap`  Data.Map.Strict.elems (_systemEffectRegistry gameState)
+      composedSystemEffects = sequence_ systemEffectComputations
+      resetSystemEffectRegistry :: GameComputation Identity ()
+      resetSystemEffectRegistry = put gameState { _systemEffectRegistry = Data.Map.Strict.empty }
+  toGameComputation sentence >> composedSystemEffects >> resetSystemEffectRegistry
 
 toGameComputation :: Sentence -> GameComputation Identity ()
 toGameComputation sentence = do
