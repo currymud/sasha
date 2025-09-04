b47ba3ed475635ba46406092d81eb300c2cabcd1
Michael Litchard <michael.litchard@platonic.systems>
2025-09-04 01:09:37 +0000

    wip

--- backend/src/Build/GameStateGeneration/SashaDemo.hs
+++ backend/src/Build/GameStateGeneration/SashaDemo.hs
@@ -6,7 +6,8 @@ import           Build.GameStateGeneration.Defaults                             
                                                                                    defaultObject,
                                                                                    defaultPlayer)
 import           Prelude                                                          hiding
-                                                                                  (take)
+                                                                                  (floor,
+                                                                                   take)
 
 -- Import semantic wrappers - DirectionalStimulus versions
 import           Grammar.Parser.Partitions.Nouns.DirectionalStimulus              (bedroomDS,
@@ -25,7 +26,7 @@ import           Model.Parser.Composites.Nouns                                  
 import           Model.GameState                                                  (AcquisitionActionF,
                                                                                    ActionEffectKey,
                                                                                    ActionManagement (AAManagementKey, AVManagementKey, DSAManagementKey, ISAManagementKey, SSAManagementKey),
-                                                                                   ActionManagementFunctions,
+                                                                                   ActionManagementFunctions (ActionManagementFunctions),
                                                                                    DirectionalStimulusActionF,
                                                                                    Effect,
                                                                                    EffectActionKey (AcquisitionalActionKey, SomaticAccessActionKey),
@@ -91,6 +92,7 @@ import           Build.BedPuzzle.Actions.Objects.Robe.Look                      
 import           Build.BedPuzzle.Actions.Open                                     (openEyes)
 import           Build.BedPuzzle.Actions.Player.Look                              (dsvActionEnabled,
                                                                                    isvActionEnabled)
+import           Build.GameStateGeneration.TestDynamicActions                     (placeObject)
 import           Control.Monad                                                    ((>=>))
 import           Data.Kind                                                        (Type)
 import qualified Data.Set
@@ -116,7 +118,6 @@ import           Model.Parser.Atomics.Nouns                                     
 import           Model.Parser.Composites.Verbs                                    (AcquisitionVerbPhrase (SimpleAcquisitionVerbPhrase),
                                                                                    ConsumptionVerbPhrase (ConsumptionVerbPhrase))
 import           Model.Parser.GCase                                               (NounKey (DirectionalStimulusKey, ObjectiveKey))
-
 -- =============================================================================
 -- VERB PHRASES
 -- =============================================================================
@@ -149,17 +150,69 @@ data ObjectBuildError
   | InvalidActionGID Text
   deriving stock (Show,Ord, Eq)
 
-buildObject :: NounPhrase DirectionalStimulus
-            -> (ActionManagementFunctions -> Either ObjectBuildError (WorldDSL Object))
-            -> ActionManagementFunctions
-            -> WorldDSL (GID Object, Object)
-buildObject nounPhrase objBuilder actions =
+sashaBedroomDemo :: WorldDSL GameState
+sashaBedroomDemo = do
+  -- Location
+  bedroomGID <- declareLocationGID (SimpleNounPhrase bedroomDS)
+
+  floorGID <- declareObjectGID (SimpleNounPhrase floorDS)
+  lookAtFloorGID <- declareDirectionalStimulusActionGID (lookAtF floorGID)
+  let floorActions = ActionManagementFunctions $ Data.Set.fromList
+        [ DSAManagementKey look lookAtFloorGID]
+      floor = buildObjectWithGID floorGID floorObj floorActions
+  registerObject floorGID floor
+  placeObject bedroomGID floorGID floorDS floorOB
+  -- Chair
+  chairGID <- declareObjectGID (SimpleNounPhrase chairDS)
+  lookAtChairGID <- declareDirectionalStimulusActionGID (lookAtF chairGID)
+  getFromChairGID <- declareAcquisitionActionGID (getFromSupportF chairGID)
+  let chairActions = ActionManagementFunctions $ Data.Set.fromList
+        [ DSAManagementKey look lookAtChairGID
+        , AVManagementKey get getFromChairGID
+        ]
+  let chair = buildObjectWithGID chairGID chairObj chairActions
+  registerObject chairGID chair
+  placeObject bedroomGID chairGID chairDS chairOB
+
+  -- Robe
+  robeGID <- declareObjectGID (SimpleNounPhrase robeDS)
+  lookAtRobeGID <- declareDirectionalStimulusActionGID (lookAtF robeGID)
+  getRobeDeniedGID <- declareAcquisitionActionGID getRobeDeniedF
+  let robeActions = ActionManagementFunctions $ Data.Set.fromList
+        [ DSAManagementKey look lookAtRobeGID
+        , AVManagementKey get getRobeDeniedGID
+        ]
+      robe = buildObjectWithGID robeGID robeObj robeActions
+  registerObject robeGID robe
+  placeObject bedroomGID robeGID robeDS robeOB
+
+
+  finalizeGameState
+
+floorObj :: ActionManagementFunctions -> Either ObjectBuildError (WorldDSL Object)
+floorObj actions = do  -- Either monad
+  lookGID <- maybeToEither (MissingAction "Missing look action for floor")
+                          (findDSAKey look actions)
+  return $ defaultObject & floorBuilder lookGID
+
+floorBuilder :: GID DirectionalStimulusActionF
+             -> Object
+             -> WorldDSL Object
+floorBuilder lookGID =
+  withShortName "floor"
+    >=> withDescription "The bedroom floor"
+    >=> withDescriptives [SimpleNounPhrase floorDS]
+    >=> (\o -> withObjectBehavior o (DSAManagementKey look lookGID))
+
+buildObjectWithGID :: GID Object
+                   -> (ActionManagementFunctions -> Either ObjectBuildError (WorldDSL Object))
+                   -> ActionManagementFunctions
+                   -> WorldDSL Object
+buildObjectWithGID gid objBuilder actions =
   case objBuilder actions of
-    Left err -> error $ "Object build failed: " ++ show err  -- Crash here!
-    Right objDSL -> do
-      gid <- declareObjectGID nounPhrase
-      obj <- objDSL
-      return (gid, obj)
+    Left err -> error $ "Object build failed for GID " ++ show gid ++ ": " ++ show err
+    Right objDSL -> objDSL
+
 
 chairObj :: ActionManagementFunctions -> Either ObjectBuildError (WorldDSL Object)
 chairObj actions = do  -- Either monad
@@ -177,6 +230,26 @@ chairBuilder lookGID getGID = withShortName "chair"
   >=> withDescriptives [SimpleNounPhrase chairDS]
   >=> (\o -> withObjectBehavior o (DSAManagementKey look lookGID))
   >=> (\o -> withObjectBehavior o (AVManagementKey get getGID))
+
+robeObj :: ActionManagementFunctions -> Either ObjectBuildError (WorldDSL Object)
+robeObj actions = do  -- Either monad
+  lookGID <- maybeToEither (MissingAction "Missing look action for robe")
+                          (findDSAKey look actions)
+  getGID <- maybeToEither (MissingAction "Missing get action for robe")
+                         (findAVKey get actions)
+  pure $ defaultObject & robeBuilder lookGID getGID
+
+robeBuilder :: GID DirectionalStimulusActionF
+            -> GID AcquisitionActionF
+            -> Object
+            -> WorldDSL Object
+robeBuilder lookGID getGID =
+  withShortName "comfortable robe"
+    >=> withDescription "The robe is draped on the chair"
+    >=> withDescriptives [SimpleNounPhrase robeDS]
+    >=> (\o -> withObjectBehavior o (DSAManagementKey look lookGID))
+    >=> (\o -> withObjectBehavior o (AVManagementKey get getGID))
+
 
 maybeToEither :: e -> Maybe a -> Either e a
 maybeToEither err Nothing = Left err
