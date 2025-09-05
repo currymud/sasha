41acdd38f5ca40ada181d52959f33c0e4fc733f2
Michael Litchard <michael.litchard@platonic.systems>
2025-09-05 19:16:19 +0000

    wip

--- backend/src/Build/GameStateGeneration/SashaDemo.hs
+++ backend/src/Build/GameStateGeneration/SashaDemo.hs
@@ -38,7 +38,7 @@ import           Model.GameState                                                
                                                                                    Player,
                                                                                    PlayerKey (PlayerKeyObject),
                                                                                    SomaticAccessActionF,
-                                                                                   SpatialRelationship (SupportedBy, Supports))
+                                                                                   SpatialRelationship (ContainedIn, SupportedBy, Supports))
 import           Model.GameState.GameStateDSL                                     (WorldDSL,
                                                                                    createAcquisitionVerbEffect,
                                                                                    createAcquisitionVerbPhraseEffect,
@@ -103,7 +103,8 @@ import qualified Data.Set
 import           Data.Text                                                        (Text)
 import           Debug.Trace                                                      (trace)
 import           GameState.ActionManagement                                       (findAVKey,
-                                                                                   findDSAKey)
+                                                                                   findDSAKey,
+                                                                                   findSAForContainersKey)
 import           Grammar.Parser.Partitions.Nouns.Consumables                      (pill,
                                                                                    pillCS)
 import           Grammar.Parser.Partitions.Nouns.Objectives                       (chairOB,
@@ -205,6 +206,7 @@ sashaBedroomDemo = do
   registerSpatial chairGID (Supports (Data.Set.singleton robeGID))
   registerSpatial chairGID (SupportedBy floorGID)
   registerSpatial robeGID (SupportedBy chairGID)
+  registerSpatial pocketGID (ContainedIn robeGID)
 
   openEyesGID <- declareSomaticActionGID openEyes
   getDeniedFGID <- declareAcquisitionActionGID getDeniedF
@@ -219,22 +221,17 @@ sashaBedroomDemo = do
   player <- buildBedroomPlayer bedroomGID isaEnabledLookGID openEyesGID dsvEnabledLookGID getDeniedFGID containerAccessDeniedFGID
   registerPlayer player
 
-  trace ("DEBUG: getRobeDeniedGID = " ++ show getRobeDeniedGID) $ pure ()
   getRobeGID <- declareAcquisitionActionGID (getObjectF robeGID)
-  trace ("DEBUG: getRobeGID = " ++ show getRobeGID) $ pure ()
-  trace ("DEBUG: getFromChairGID = " ++ show getFromChairGID) $ pure ()
   -- Effects
   openEyesLookChangeEffect <- createImplicitStimulusEffect isaLook lookFGID
   linkEffectToLocation (SomaticAccessActionKey openEyesGID) bedroomGID openEyesLookChangeEffect
 
   robeOpenEyesLookChangesGetRobeForPlayer <- createAcquisitionVerbPhraseEffect getRobeAVP playerGetFGID
   linkEffectToPlayer (SomaticAccessActionKey openEyesGID) (PlayerKeyObject robeGID) robeOpenEyesLookChangesGetRobeForPlayer
-
 --  registerSpatial tableGID (Supports (Set.singleton mailGID))
 --  registerSpatial tableGID (SupportedBy floorGID)
 --  registerSpatial mailGID (SupportedBy tableGID)
 --  registerSpatial robeGID (Contains (Data.Set.singleton pocketGID))
---  registerSpatial pocketGID (ContainedIn robeGID)
 --  registerSpatial pocketGID (Contains (Set.singleton pillGID))
 --  registerSpatial pillGID (ContainedIn pocketGID)
 --  registerSpatial floorGID (Supports (Data.Set.fromList [chairGID, tableGID]))
@@ -330,168 +327,26 @@ robeBuilder lookGID getGID =
     >=> (\o -> withObjectBehavior o (DSAManagementKey look lookGID))
     >=> (\o -> withObjectBehavior o (AVManagementKey get getGID))
 
+pocketObj :: ActionManagementFunctions -> Either ObjectBuildError (WorldDSL Object)
+pocketObj actions = do  -- Either monad
+  lookGID <- maybeToEither (MissingAction "Missing look action for pocket")
+                          (findDSAKey look actions)
+  openGID <- maybeToEither (MissingAction "Missing open action for pocket")
+                          (findSAForContainersKey openSA actions)
+  pure $ defaultObject & pocketBuilder lookGID openGID
+
+pocketBuilder :: GID DirectionalStimulusActionF
+              -> GID ContainerAccessActionF
+              -> Object
+              -> WorldDSL Object
+pocketBuilder lookGID openGID =
+  withShortName "pocket"
+    >=> withDescription "A pocket sewn into the robe"
+    >=> withDescriptives [SimpleNounPhrase pocketDS]
+    >=> (\o -> withObjectBehavior o (DSAManagementKey look lookGID))
+    >=> (\o -> withObjectBehavior o (SAConManagementKey openSA openGID))
+
 
 maybeToEither :: e -> Maybe a -> Either e a
 maybeToEither err Nothing = Left err
 maybeToEither _ (Just a)  = Right a
-
-
-    {-
-testDynamicActionsDS :: WorldDSL GameState
-testDynamicActionsDSL = do
-  -- Declare location GID
-  bedroomGID <- declareLocationGID (SimpleNounPhrase bedroomDS)
-
--- Object GIDs
-  floorGID <- declareObjectGID (SimpleNounPhrase floorDS)
-  chairGID <- declareObjectGID (SimpleNounPhrase chairDS)
-  robeGID <- declareObjectGID (SimpleNounPhrase robeDS)
-  pocketGID <- declareObjectGID (SimpleNounPhrase pocketDS)
-
-  lookAtChairFGID <- declareDirectionalStimulusActionGID (lookAtF chairGID)
-  whatChairGID <- declareDirectionalStimulusActionGID whatChairF
-  getFromChairFGID <- declareAcquisitionActionGID (getFromSupportF chairGID)
-
-  whatPocketFGID <- declareDirectionalStimulusActionGID whatPocket
-  lookAtPocketFGID <- declareDirectionalStimulusActionGID (lookAtF pocketGID)
-
-  lookAtRobeFGID <- declareDirectionalStimulusActionGID (lookAtF robeGID)
-  notEvenRobeFGID <- declareDirectionalStimulusActionGID notEvenRobeF
-  getRobeDeniedFGID <- declareAcquisitionActionGID getRobeDeniedF
-  getRobeFGID <- declareAcquisitionActionGID (getObjectF robeGID)
-
-  lookFloorGID <- declareDirectionalStimulusActionGID (lookAtF floorGID)
-
-  whatPillFGID <- declareDirectionalStimulusActionGID whatPill
-
-  registerObject chairGID (chairObj whatChairGID getFromChairFGID)
-  registerObject floorGID (floorObj lookFloorGID)
-  registerObject robeGID (robeObj notEvenRobeFGID getRobeDeniedFGID)
-  registerObject pocketGID (pocketObj whatPocketFGID)
-
-  openEyesGID <- declareSomaticActionGID openEyes
-  getDeniedFGID <- declareAcquisitionActionGID getDeniedF
-  playerGetFGID <- declareAcquisitionActionGID getF
-  pitchBlackGID <- declareImplicitStimulusActionGID pitchBlackF
-  lookFGID <- declareImplicitStimulusActionGID lookF
-  isaEnabledLookGID <- declareImplicitStimulusActionGID (isvActionEnabled isaLook)
-  dsvEnabledLookGID <- declareDirectionalStimulusActionGID dsvActionEnabled
-  accessContainerDeniedFGID <- declareContainerAccessActionGID openDeniedF
-
-  registerLocation bedroomGID (buildLocation pitchBlackGID)
-
-  placeObject bedroomGID chairGID chairDS chairOB
-  placeObject bedroomGID floorGID floorDS floorOB
-  placeObject bedroomGID robeGID robeDS robeOB
-
-  registerSpatial chairGID (SupportedBy floorGID)
-  registerSpatial floorGID (Supports (Data.Set.singleton chairGID))
-  registerSpatial chairGID (Supports (Data.Set.singleton robeGID))
-  registerSpatial robeGID (Supports (Data.Set.singleton pocketGID))
-  registerSpatial robeGID (SupportedBy chairGID)
-  registerSpatial pocketGID (SupportedBy robeGID)
---  registerObject floorGID (floorObj id)
-  player <- buildBedroomPlayer bedroomGID isaEnabledLookGID openEyesGID dsvEnabledLookGID getDeniedFGID accessContainerDeniedFGID
-
-  openEyesLookChangeEffect <- createImplicitStimulusEffect isaLook lookFGID
-  linkEffectToLocation (SomaticAccessActionKey openEyesGID) bedroomGID openEyesLookChangeEffect
-
-  openEyesLookChangesLookChair <- createDirectionalStimulusEffect look lookAtChairFGID
-  linkEffectToObject (SomaticAccessActionKey openEyesGID) chairGID openEyesLookChangesLookChair
-
-  robeOpenEyesLookChangesLookRobe <- createDirectionalStimulusEffect look lookAtRobeFGID
-  linkEffectToObject ( SomaticAccessActionKey openEyesGID) robeGID robeOpenEyesLookChangesLookRobe
-
-  openEyesLookChangesGetRobe <- createAcquisitionVerbPhraseEffect getRobeAVP getRobeFGID
-  linkEffectToObject ( SomaticAccessActionKey openEyesGID) robeGID openEyesLookChangesGetRobe
-
-  robeOpenEyesLookChangesGetRobeForPlayer <- createAcquisitionVerbPhraseEffect getRobeAVP playerGetFGID
-  linkEffectToPlayer (SomaticAccessActionKey openEyesGID) (PlayerKeyObject robeGID) robeOpenEyesLookChangesGetRobeForPlayer
-
-  openEyesChangeGetVerb <- createAcquisitionVerbEffect get getRobeFGID
-  linkEffectToObject (SomaticAccessActionKey openEyesGID) robeGID openEyesChangeGetVerb
-
-  getRobeChangesLookAtPocket <- createDirectionalStimulusEffect look lookAtPocketFGID
-  linkEffectToObject (AcquisitionalActionKey getRobeFGID) pocketGID getRobeChangesLookAtPocket
-
-  robeHoldingDescriptionEffect <- updateDescription "A comfortable robe you are holding. You notice it has a pocket - that seems important for some reason." robeGID
-  linkFieldEffectToObject (AcquisitionalActionKey getRobeFGID) robeGID robeHoldingDescriptionEffect
-
-  registerPlayer player
-  setPerceptionMap
-    [ (DirectionalStimulusNounPhrase at (SimpleNounPhrase chairDS), [chairGID])
---    , (DirectionalStimulusNounPhrase (SimpleNounPhrase tableDS), [tableGID])
-    , (DirectionalStimulusNounPhrase at (SimpleNounPhrase robeDS), [robeGID])
---    , (DirectionalStimulusNounPhrase (SimpleNounPhrase mailDS), [mailGID])
-    , (DirectionalStimulusNounPhrase at (SimpleNounPhrase floorDS), [floorGID])
-    ]
-
-  finalizeGameState
-
-buildLocation :: GID ImplicitStimulusActionF -> WorldDSL Location
-buildLocation pitchBlackGID = defaultLocation & bedroomLoc'
-  where
-    bedroomLoc' = withTitle "bedroom in bed"
-                    >=> (\l -> withLocationBehavior l (ISAManagementKey isaLook pitchBlackGID))
-
-buildBedroomPlayer :: GID Location
-                        -> GID ImplicitStimulusActionF
-                        -> GID SomaticAccessActionF
-                        -> GID DirectionalStimulusActionF
-                        -> GID AcquisitionActionF
-                        -> WorldDSL Player
-buildBedroomPlayer bedroomGID isaEnabledLookGID openEyesGID dsaEnabledLookGID getRobeFGID =
-  withPlayerLocation defaultPlayer bedroomGID
-    >>= (\p -> withPlayerBehavior p (ISAManagementKey isaLook isaEnabledLookGID))
-    >>= (\p -> withPlayerBehavior p (DSAManagementKey look dsaEnabledLookGID))
-    >>= (\p -> withPlayerBehavior p (SSAManagementKey saOpen openEyesGID))
-    >>= (\p -> withPlayerBehavior p (ISAManagementKey isaLook isaEnabledLookGID))
-    >>= (\p -> withPlayerBehavior p (AAManagementKey getRobeAVP getRobeFGID))
-    >>= (\p -> withPlayerBehavior p (AVManagementKey get getRobeFGID))
-
-robeObj :: GID DirectionalStimulusActionF  -- Eyes closed look response
-        -> GID AcquisitionActionF          -- Can't get (eyes closed/not standing)
-        -> WorldDSL Object
-robeObj cannotSeeRobeGID cannotGetRobeGID =
-  defaultObject & robeObj'
-  where
-    robeObj' = withShortName "comfortable robe"
-                 >=> withDescription "The robe is draped on the chair"
-                 >=> withDescriptives [SimpleNounPhrase robeDS]
-                 >=> (\o -> withObjectBehavior o (DSAManagementKey look cannotSeeRobeGID))
-                 >=> (\o -> withObjectBehavior o (AVManagementKey get cannotGetRobeGID))
-                 >=> (\o -> withObjectBehavior o (AAManagementKey getRobeAVP cannotGetRobeGID))
-
-chairObj :: GID DirectionalStimulusActionF
-              -> GID AcquisitionActionF
-              -> WorldDSL Object
-chairObj lookResponseGID getResponseFGID = defaultObject & chairObj'
-  where
-    chairObj' = withShortName "chair"
-                  >=> withDescription "A simple wooden chair"
-                  >=> withDescriptives [SimpleNounPhrase chairDS]
-                  >=> (\o -> withObjectBehavior o (DSAManagementKey look lookResponseGID))
-                  >=> (\o -> withObjectBehavior o (AVManagementKey  get  getResponseFGID))
-
-pocketObj :: GID DirectionalStimulusActionF -> WorldDSL Object
-pocketObj lookResponseGID = defaultObject & pocketObj'
-  where
-    pocketObj' = withShortName "pocket"
-                   >=> withDescription "There's something in the pocket"
-                   >=> withDescriptives [SimpleNounPhrase pocketDS]
-                   >=> (\o -> withObjectBehavior o (DSAManagementKey look lookResponseGID))
-
-
-placeObject :: GID Location -> GID Object -> DirectionalStimulus -> Objective -> WorldDSL ()
-placeObject lid oid ds obj = do
-  registerObjectToLocation lid oid (DirectionalStimulusKey ds)
-  registerObjectToLocation lid oid (ObjectiveKey obj)
-
-floorObj :: GID DirectionalStimulusActionF -> WorldDSL Object
-floorObj lookFloorGID = defaultObject & floorObj'
-  where
-    floorObj' = withShortName "floor"
-                  >=> withDescription "The bedroom floor"
-                  >=> withDescriptives [SimpleNounPhrase floorDS]
-                  >=> (\o -> withObjectBehavior o (DSAManagementKey look lookFloorGID))
-                  -}
