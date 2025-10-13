c81a77f2c77280ff3d610e0b9710b81d928b0145
Michael Litchard <michael.litchard@platonic.systems>
2025-10-13 17:11:41 +0000

    wip

--- backend/src/ConstraintRefinement/Actions/Objects/Get/Constructors.hs
+++ backend/src/ConstraintRefinement/Actions/Objects/Get/Constructors.hs
@@ -7,6 +7,8 @@ import           GameState                                        (addToInventor
 import           GameState.ActionManagement                       (processEffectsFromRegistry)
 import           Grammar.Parser.Partitions.Verbs.AcquisitionVerbs (get)
 import           Model.Core                                       (AcquisitionActionF (CollectedF, LosesObjectF, ObjectNotGettableF),
+                                                                   ObjectAcquisitionActionF,
+                                                                   ContainerAcquisitionActionF,
                                                                    ActionEffectKey (AcquisitionalActionKey),
                                                                    ActionManagement (AVManagementKey),
                                                                    ActionManagementFunctions (ActionManagementFunctions),
@@ -15,55 +17,15 @@ import           Model.Core                                       (AcquisitionAc
                                                                    Object (_objectActionManagement),
                                                                    SpatialRelationship (ContainedIn, Contains, SupportedBy, Supports))
 import           Model.GID                                        (GID)
+-- Import role-based constructors and conversion functions
+import           ConstraintRefinement.Actions.RoleBased.Constructors (objectCollectedF, objectNotCollectableF, containerLosesObjectF)
+import           ConstraintRefinement.Actions.RoleBased.Conversions (objectToAcquisitionActionF, containerToAcquisitionActionF)
 
 getObjectF :: GID Object -> AcquisitionActionF
-getObjectF objectGID = CollectedF getit
-  where
-    getit :: GameComputation Identity CoordinationResult
-    getit = do
-      actionManagement <- _objectActionManagement <$> getObjectM objectGID
-      let ActionManagementFunctions actionSet = actionManagement
-      -- Find the single AVManagementKey entry that matches the 'get' verb
-      let getActionGIDs = [gid | AVManagementKey verb gid <- Data.Set.toList actionSet, verb == get]
-      pure $ CoordinationResult
-        { _computation = addToInventoryM objectGID
-        , _actionEffectKeys = map AcquisitionalActionKey getActionGIDs
-        }
+getObjectF objectGID = objectToAcquisitionActionF (objectCollectedF objectGID)
 
 getFromSupportF :: GID Object -> AcquisitionActionF
-getFromSupportF supportObjGID = LosesObjectF getit
-  where
-    getit :: GID Object
-              -> GameComputation Identity CoordinationResult
-    getit targetObjectGID = do
-      -- Get the target object's action management
-      actionManagement <- _objectActionManagement <$> getObjectM targetObjectGID
-      let ActionManagementFunctions actionSet = actionManagement
-      -- Find the AVManagementKey entry that matches the 'get' verb
-      let getActionGIDs = [gid | AVManagementKey verb gid <- Data.Set.toList actionSet, verb == get]
-
-      let computation = do
-            -- Remove spatial relationships from target object (robe)
-            modifySpatialRelationshipsForObjectM targetObjectGID $ \rels ->
-              Data.Set.filter (\case
-                SupportedBy oid -> oid /= supportObjGID
-                ContainedIn oid -> oid /= supportObjGID
-                _ -> True) rels
-
-            -- Remove target object from support object's relationships (chair)
-            modifySpatialRelationshipsForObjectM supportObjGID $ \rels ->
-              Data.Set.map (\case
-                Supports objSet -> Supports (Data.Set.delete targetObjectGID objSet)
-                Contains objSet -> Contains (Data.Set.delete targetObjectGID objSet)
-                other -> other) rels
-
-            -- Add to inventory
-            addToInventoryM targetObjectGID
-
-      pure $ CoordinationResult
-        { _computation = computation
-        , _actionEffectKeys = map AcquisitionalActionKey getActionGIDs
-        }
+getFromSupportF supportObjGID = containerToAcquisitionActionF (containerLosesObjectF supportObjGID)
 
 objectNotGettableF :: AcquisitionActionF
-objectNotGettableF = ObjectNotGettableF processEffectsFromRegistry
+objectNotGettableF = objectToAcquisitionActionF objectNotCollectableF
