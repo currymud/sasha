dca340747bdbf61282e67651b446c03c13b42934
Michael Litchard <michael.litchard@platonic.systems>
2025-10-17 08:45:01 +0000

    moar cruft

--- backend/src/ConstraintRefinement/Actions/Locations/Look.hs
+++ backend/src/ConstraintRefinement/Actions/Locations/Look.hs
@@ -1,30 +1,11 @@
 module ConstraintRefinement.Actions.Locations.Look where
-import           Control.Monad.Identity     (Identity)
 import           GameState.ActionManagement (processEffectsFromRegistry)
-import           Model.Core                 (ActionEffectKey,
-                                             AgentImplicitStimulusActionF (AgentCannotSeeF),
-                                             GameComputation,
-                                             ImplicitStimulusActionF (CannotImplicitStimulusActionF, PlayerImplicitStimulusActionF),
-                                             LocationDirectionalStimulusActionF (LocationCanBeSeenF, LocationCannotBeSeenF),
+import           Model.Core                 (LocationDirectionalStimulusActionF (LocationCanBeSeenF, LocationCannotBeSeenF),
                                              LocationDirectionalStimulusContainerActionF (LocationCanBeSeenInF, LocationCannotBeSeenInF),
                                              LocationImplicitStimulusActionF (LocationCanBeSeenImplicitF, LocationCannotBeSeenImplicitF))
--- deprecated
-pitchBlackF :: ImplicitStimulusActionF
-pitchBlackF = CannotImplicitStimulusActionF pitchBlack'
-  where
-    pitchBlack' :: ActionEffectKey -> GameComputation Identity ()
-    pitchBlack' actionEffectKey = do
-      processEffectsFromRegistry actionEffectKey
 
 locationNoSeeF :: LocationImplicitStimulusActionF
 locationNoSeeF = LocationCannotBeSeenImplicitF processEffectsFromRegistry
-
-lookF :: ImplicitStimulusActionF
-lookF = PlayerImplicitStimulusActionF look
-  where
-    look :: ActionEffectKey -> GameComputation Identity ()
-    look actionEffectKey = do
-      processEffectsFromRegistry actionEffectKey
 
 allowLookF :: LocationImplicitStimulusActionF
 allowLookF = LocationCanBeSeenImplicitF processEffectsFromRegistry
